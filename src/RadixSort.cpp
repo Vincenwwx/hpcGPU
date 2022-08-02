c142ebd9d78794f9b581a3ca13c6393c9e45d370
Vincenwwx <berneywwx@gmail.com>
2022-08-02 21:03:35 +0200

    Add function to find kernel path automatically

--- src/RadixSort.cpp
+++ src/RadixSort.cpp
@@ -28,8 +28,16 @@ const unsigned int DIGIT_SIZE = 8;
 const unsigned int DIGIT_PLACE = KEY_SIZE / DIGIT_SIZE;
 const unsigned int NUM_DIGIT_BINNINGS = 1 << DIGIT_SIZE;
 
-const unsigned int count = 2048*2048*4; // lenght of Array with data to sort, 1024*1024*4
+const unsigned int count = 2048*4086*4; // lenght of Array with data to sort, 1024*1024*4
 const unsigned int num_data_per_work_item = 4; // use a unsigned int vector of length 4
+
+/**
+Find the path of a file
+	@param filename: the name of the file to find
+	@param executablePath optional absolute path of the executable
+*/
+char* findFilePath(const char* filename, const char* executablePath);
+
 
 //////////////////////////////////////////////////////////////////////////////
 // CPU implementation
@@ -142,7 +150,13 @@ int main(int argc, char** argv) {
 	
 	// Load the source code
 	// TODO: change the path of kernel file
-	cl::Program program = OpenCL::loadProgramSource(context, "/scratch/radixSort/src/radixSort.cl");
+	char* kernelFilePath = findFilePath("radixSort.cl", argv[0]);
+	if (kernelFilePath == nullptr) {
+		std::cerr << "Can't find kernel..." << std::endl;
+		exit(1);
+	}
+	//cl::Program program = OpenCL::loadProgramSource(context, "/scratch/radixSort/src/radixSort.cl");
+	cl::Program program = OpenCL::loadProgramSource(context, kernelFilePath);
 	// Compile the source code. This is similar to program.build(devices) but will print more detailed error messages
 	OpenCL::buildProgram(program, devices);
 
@@ -335,5 +349,114 @@ int main(int argc, char** argv) {
 
 	std::cout << "Success!" << std::endl;
 
+	return 0;
+}
+
+char* findFilePath(const char* filename, const char* executable_path) 
+{
+	// <executable_name> defines a variable that is replaced with the name of the executable
+
+	// Typical relative search paths to locate needed companion files (e.g. sample input data, or JIT source files)
+	// The origin for the relative search may be the .exe file, a .bat file launching an .exe, a browser .exe launching the .exe or .bat, etc
+	const char* searchPath[] = 
+	{
+		"./",                                       // same dir 
+		"./data/",                                  // "/data/" subdir 
+		"./src/",                                   // "/src/" subdir
+		"./src/<executable_name>/data/",            // "/src/<executable_name>/data/" subdir 
+		"./inc/",                                   // "/inc/" subdir
+		"../",                                      // up 1 in tree 
+		"../data/",                                 // up 1 in tree, "/data/" subdir 
+		"../src/",                                  // up 1 in tree, "/src/" subdir 
+		"../inc/",                                  // up 1 in tree, "/inc/" subdir 
+		"../OpenCL/src/<executable_name>/",         // up 1 in tree, "/OpenCL/src/<executable_name>/" subdir 
+		"../OpenCL/src/<executable_name>/data/",    // up 1 in tree, "/OpenCL/src/<executable_name>/data/" subdir 
+		"../OpenCL/src/<executable_name>/src/",     // up 1 in tree, "/OpenCL/src/<executable_name>/src/" subdir 
+		"../OpenCL/src/<executable_name>/inc/",     // up 1 in tree, "/OpenCL/src/<executable_name>/inc/" subdir 
+		"../C/src/<executable_name>/",              // up 1 in tree, "/C/src/<executable_name>/" subdir 
+		"../C/src/<executable_name>/data/",         // up 1 in tree, "/C/src/<executable_name>/data/" subdir 
+		"../C/src/<executable_name>/src/",          // up 1 in tree, "/C/src/<executable_name>/src/" subdir 
+		"../C/src/<executable_name>/inc/",          // up 1 in tree, "/C/src/<executable_name>/inc/" subdir 
+		"../DirectCompute/src/<executable_name>/",      // up 1 in tree, "/DirectCompute/src/<executable_name>/" subdir 
+		"../DirectCompute/src/<executable_name>/data/", // up 1 in tree, "/DirectCompute/src/<executable_name>/data/" subdir 
+		"../DirectCompute/src/<executable_name>/src/",  // up 1 in tree, "/DirectCompute/src/<executable_name>/src/" subdir 
+		"../DirectCompute/src/<executable_name>/inc/",  // up 1 in tree, "/DirectCompute/src/<executable_name>/inc/" subdir 
+		"../../",                                   // up 2 in tree 
+		"../../data/",                              // up 2 in tree, "/data/" subdir 
+		"../../src/",                               // up 2 in tree, "/src/" subdir 
+		"../../inc/",                               // up 2 in tree, "/inc/" subdir 
+		"../../../",                                // up 3 in tree 
+		"../../../src/<executable_name>/",          // up 3 in tree, "/src/<executable_name>/" subdir 
+		"../../../src/<executable_name>/data/",     // up 3 in tree, "/src/<executable_name>/data/" subdir 
+		"../../../src/<executable_name>/src/",      // up 3 in tree, "/src/<executable_name>/src/" subdir 
+		"../../../src/<executable_name>/inc/",      // up 3 in tree, "/src/<executable_name>/inc/" subdir 
+		"../../../sandbox/<executable_name>/",      // up 3 in tree, "/sandbox/<executable_name>/" subdir
+		"../../../sandbox/<executable_name>/data/", // up 3 in tree, "/sandbox/<executable_name>/data/" subdir
+		"../../../sandbox/<executable_name>/src/",  // up 3 in tree, "/sandbox/<executable_name>/src/" subdir
+		"../../../sandbox/<executable_name>/inc/"   // up 3 in tree, "/sandbox/<executable_name>/inc/" subdir
+	};
+    
+	// Extract the executable name
+	std::string executable_name;
+	if (executable_path != 0) 
+	{
+		executable_name = std::string(executable_path);
+
+	#ifdef _WIN32        
+		// Windows path delimiter
+		size_t delimiter_pos = executable_name.find_last_of('\\');        
+		executable_name.erase(0, delimiter_pos + 1);
+
+		if (executable_name.rfind(".exe") != string::npos)
+		{
+			// we strip .exe, only if the .exe is found
+			executable_name.resize(executable_name.size() - 4);        
+		}
+	#else
+		// Linux & OSX path delimiter
+		size_t delimiter_pos = executable_name.find_last_of('/');
+		executable_name.erase(0, delimiter_pos+1);
+	#endif
+	}
+    
+	// Loop over all search paths and return the first hit
+	for(unsigned int i = 0; i < sizeof(searchPath)/sizeof(char*); ++i)
+	{
+		std::string path(searchPath[i]);        
+		size_t executable_name_pos = path.find("<executable_name>");
+
+		// If there is executable_name variable in the searchPath 
+		// replace it with the value
+		if(executable_name_pos != std::string::npos)
+		{
+			if(executable_path != 0) 
+			{
+				path.replace(executable_name_pos, strlen("<executable_name>"), executable_name);
+			} 
+			else 
+			{
+				// Skip this path entry if no executable argument is given
+				continue;
+			}
+		}
+        
+		// Test if the file exists
+		path.append(filename);
+		std::fstream fh(path.c_str(), std::fstream::in);
+		if (fh.good())
+		{
+			// File found
+			// returning an allocated array here for backwards compatibility reasons
+			char* file_path = (char*) malloc(path.length() + 1);
+		#ifdef _WIN32  
+			strcpy_s(file_path, path.length() + 1, path.c_str());
+		#else
+			strcpy(file_path, path.c_str());
+		#endif                
+			return file_path;
+		}
+	}    
+
+	// File not found
 	return 0;
 }
